26d52b7217e17b3db50983ead1854e3e2660aba0
unifat <38376458+unifat@users.noreply.github.com>
2018-04-14 21:08:16 +0800

    page-header

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title></title>
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark .fixed-top">
+      <a class="navbar-brand" href="#">UniFatale</a>
+    <!-- Logo Image here
+    <img src="#" alt="Logo" style="width:40px;">
+    -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Market</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Industry</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Characters</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
