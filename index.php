38f7c8ff325891fe2e154ac6776ca2c08884d948
unifat <38376458+unifat@users.noreply.github.com>
2018-04-14 21:12:02 +0800

    index

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include_once 'header.php';
+ ?>
+  <section class="main-container">
+    <div class="main-wrapper" align="center">
+      <h2>Home</h2>
+    </div>
+  </section>
+  <?php
+  include_once 'footer.php';
+   ?>
